12dc83443e3ed5882256efa0c3521bc70c9faa77
Michael Grantham <MichaelGrantham@live.com>
2022-03-10 22:59:21 -0600

    A little more code

--- Project code.R
+++ Project code.R
@@ -1,7 +1,6 @@
 library(tidytext)
 library(tidyverse)
-library(superml)
-
+library(quanteda)
 
 train<-read.csv("train.csv")
 test<-read.csv("test.csv")
@@ -43,12 +42,63 @@ txt %>%
        subtitle = "using Tidytext in R",
        caption = "Data Source: itunesr - iTunes App Store")
 
-#
+# read in training data
+train <- read.csv("train.csv")
+test <- read.csv("test.csv")
 
-cfv <- CountVectorizer$new(remove_stopwords = TRUE,
-                           ngram_range = c(1, 2))
-cf_mat <- cfv$fit_transform(txt)
+# combine training and testing data to produce unified corpus
+tweets <- rbind(train[,-ncol(train)], test)
 
+# get indices specifying start and stop of train/test data
+# use to subset when training and testing classifiers
+index_train <- c(1, nrow(train))
+index_test <- c((nrow(train) + 1), (nrow(train) + nrow(test)))
 
+# make tokens
+tweet_corpus <- corpus(char_tolower(tweets$text))
 
-mtcars.pca <- prcomp(cf_mat, center = TRUE,scale. = TRUE)
+# remove punctuation, urls, and symbols
+# note, we are still able to keep hashtags and usernames
+tweet_tokens <- tokens(tweet_corpus, 
+                       remove_punct = TRUE, remove_url = TRUE, remove_symbols = TRUE)
+
+tweet_tokens_no_stop <- tokens_remove(tweet_tokens, pattern = stopwords("en"))
+
+# get unigrams and bigrams
+tweet_ngrams <- tokens_ngrams(x = tweet_tokens_no_stop, n = 1:2)
+
+# make the design matrix (i.e., document term matrix)
+tweet_dfmat <- dfm(tweet_ngrams)
+dim(tweet_dfmat)
+
+# split training set into training and test set
+set.seed(123)
+prop_train <- 0.2
+train_ids <- sample(1:nrow(train), size = round(prop_train * nrow(train)),
+                    replace = FALSE)
+test_ids <- (index_train[1]:index_train[2])[-train_ids]
+foldid <- sample(rep(1:10,length.out = length(train_ids))) # control which obs go to which folds in CV
+
+# subset design matrix according to train-test
+tweet_train <- tweet_dfmat[train_ids,]
+tweet_test <- tweet_dfmat[test_ids,]
+
+# subset known target values 
+tweet_train_target <- train$target[train_ids]
+tweet_test_target <- train$target[test_ids]
+
+nrow(tweet_train) == length(tweet_train_target)
+## [1] TRUE
+nrow(tweet_test) == length(tweet_test_target)
+
+pc <- prcomp(tweet_train,
+             center = TRUE,
+             scale. = F)
+
+trg <- predict(pc, tweet_train)
+
+#Add the target column information into trg.
+
+trg <- data.frame(trg, tweet_train_target)
+tst <- predict(pc, tweet_test)
+tst <- data.frame(tst, tweet_train_target)
